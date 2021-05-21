c397f0418d9bdd6f7aaeb8423e694c83158132d9
Jaesik Kim <tensaijskim1991@gmail.com>
2021-05-22 00:35:09 +0900

    in progress

---
+++ react-food-order-app/.prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    jsxSingleQuote: false, // JSX에서 singleQuote or doubleQuote
+    trailingComma: 'all', // comma 항상 붙이기
+    bracketSpacing: true, // 객체리터럴에서 { } 사이에 공백을 넣을 것인지
+}
